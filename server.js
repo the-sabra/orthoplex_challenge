5446ec16e70068f0c6b4f4291f5c8e10053e0b43
the-sabra <omarsabra509@gmail.com>
2025-02-01 20:06:25 +0200

    chore: Add express-rate-limit middleware to limit repeated requests and prevent normal DDoS Attach

--- server.js
+++ server.js
@@ -7,26 +7,35 @@ import { errorHandler } from './middleware/error.handler.js';
 import helmet from 'helmet';
 import cors from 'cors';
 import morgan from 'morgan';
+import { rateLimit } from 'express-rate-limit'
 
 const app = express();
 dotenv.config(); 
 
-const port = process.env.PORT || 3000;
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 50 // limit each IP to 50 requests per windowMs
+  });
  
+const port = process.env.PORT || 3000;
+  
 // Middleware for logging HTTP requests
 app.use(morgan('combined'));
-
+  
 // Middleware for parsing JSON bodies
 app.use(express.json());
-
+  
 // Middleware for enabling CORS
 app.use(cors({
-    origin:"*"
-}));
+      origin:"*"
+    }));
 
+// Middleware for limiting repeated requests
+app.use(limiter); 
+    
 // Middleware for adding Helmet security headers
 app.use(helmet());
- 
+    
 app.use('/auth', AuthRouter); 
 app.use('/users', UserRouter);
 
